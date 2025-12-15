3925bb9b79f718e3bfe2c0d186fb48765677c50b
Alexandr <ua.oleksandr.feshchenko@gmail.com>
2025-12-15 15:37:32 +0200

    Add redux

--- App.tsx
+++ App.tsx
@@ -1,39 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {Provider} from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar, StyleSheet, useColorScheme, View} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { store } from "./src/store";
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+        <Provider store={store}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <AppContent />
+        </Provider>
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
